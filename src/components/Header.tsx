32c2ee13dc4a2ed6f30b7d302a1cd07a0fbc38b4
elen <elenndev@gmail.com>
2025-09-18 14:53:55 -0300

    style: details on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ import ThemeToggle from "./ThemeToggle"
 
 export default function Header() {
   return (
-    <header className="relative w-[98%] md:w-[90%] bg-body flex items-center justify-between px-6 py-4 shadow-md border-1 border-default">
-      <h1 className="text-xl font-bold uppercase">Elenndev</h1>
+    <header className="w-[98%] md:w-[90%] mt-2 bg-body flex items-center justify-between px-6 py-4 shadow-md border-1 border-default sticky">
+      <p className="text-4xl
+        font-extrabold uppercase">Elenndev</p>
       <span className="flex flex-row gap-8 md: px-2">
         <ThemeToggle />
         <Navbar />
