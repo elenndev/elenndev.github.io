e02dd4f2f8467390323966aa676d1085a64aa11b
elen <elenndev@gmail.com>
2025-09-22 11:11:44 -0300

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/elenndev.github.io/",
+  base: "./elenndev.github.io/",
   plugins: [react(), tailwindcss()],
 })
